c0204beee078be109b78c69470b0587491eebbcd
Marc Altmann <marcaltmann@posteo.de>
2017-09-23 00:45:09 +0200

    Test deep-freeze further

--- src/__tests__/deep-freeze.js
+++ src/__tests__/deep-freeze.js
@@ -1,7 +1,7 @@
 import deepFreeze from 'deep-freeze';
 
 describe('deep-freeze', () => {
-  it('should throw', () => {
+  it('should freeze first argument', () => {
     const test = {
       monday: {
         tasks: {
@@ -18,5 +18,33 @@ describe('deep-freeze', () => {
     }
 
     expect(mutation).toThrow();
+  });
+
+  it('should not freeze any other arguments', () => {
+    const test1 = {
+      monday: {
+        tasks: {
+          1: 'eat',
+          2: 'sleep',
+        },
+      },
+    };
+
+    const test2 = {
+      tuesday: {
+        tasks: {
+          1: 'nothing',
+          2: 'drink tea',
+        },
+      },
+    };
+
+    deepFreeze(test1, test2);
+
+    function mutation() {
+      test2.tuesday.tasks[1] = 'buy tea';
+    }
+
+    expect(mutation).not.toThrow();
   });
 });
