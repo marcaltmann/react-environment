3df42cae7f36fdc0e2999601e663d40e522e731c
Marc Altmann <marcaltmann@posteo.de>
2017-10-01 15:31:18 +0200

    Finish promise test examples

--- src/__tests__/promises.js
+++ src/__tests__/promises.js
@@ -8,17 +8,60 @@ describe('Promises', () => {
       }
     });
 
-  test('fulfilling promise', () => {
-    const promise = createPromise(true);
-    return promise.then(res => {
-      expect(res).toEqual('Promise fulfilled');
+  describe('standard syntax', () => {
+    test('fulfilling promise', () => {
+      const promise = createPromise(true);
+      return promise.then(res => {
+        expect(res).toEqual('Promise fulfilled');
+      });
+    });
+
+    test('rejecting promise', () => {
+      const promise = createPromise(false);
+      return promise.then(undefined, err => {
+        expect(err).toEqual(new Error('Promise rejected'));
+      });
     });
   });
 
-  test('rejecting promise', () => {
-    const promise = createPromise(false);
-    return promise.then(undefined, err => {
-      expect(err).toEqual(new Error('Promise rejected'));
+  describe('with async', () => {
+    test('fulfilling promise', async () => {
+      const promise = createPromise(true);
+      const result = await promise;
+      expect(result).toEqual('Promise fulfilled');
+    });
+
+    test('rejecting promise', async () => {
+      const promise = createPromise(false);
+      try {
+        await promise;
+      } catch (e) {
+        expect(e).toEqual(new Error('Promise rejected'));
+      }
+    });
+  });
+
+  describe('with matchers', () => {
+    test('fulfilling promise', () => {
+      const promise = createPromise(true);
+      return expect(promise).resolves.toEqual('Promise fulfilled');
+    });
+
+    test('rejecting promise', () => {
+      const promise = createPromise(false);
+      return expect(promise).rejects.toEqual(new Error('Promise rejected'));
+    });
+  });
+
+  describe('with matchers and async', () => {
+    test('fulfilling promise', async () => {
+      const promise = createPromise(true);
+      await expect(promise).resolves.toEqual('Promise fulfilled');
+    });
+
+    test('rejecting promise', async () => {
+      const promise = createPromise(false);
+      await expect(promise).rejects.toEqual(new Error('Promise rejected'));
     });
   });
 });
