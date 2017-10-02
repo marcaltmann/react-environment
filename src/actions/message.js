489269ff9b06cd2c452a59de6893a6c008932dcf
Marc Altmann <marcaltmann@posteo.de>
2017-10-03 00:09:40 +0200

    Syntax example

--- src/actions/message.js
+++ src/actions/message.js
@@ -1,21 +1,20 @@
 import * as types from '../constants/types.js';
 import * as api from '../api';
 
-export function fetchMessage() {
-  return (dispatch) => {
-    dispatch({
-      type: types.message.FETCH_REQUEST,
-    });
-    return api.fetchMessage()
-      .then(response => dispatch({
-        type: types.message.FETCH_SUCCESS,
-        payload: {
-          message: response.message,
-        },
-      }))
-      .catch(ex => dispatch({
-        type: types.message.FETCH_FAILURE,
-        payload: ex,
-      }));
-  };
-}
+export const fetchMessage = () => (dispatch) => {
+  dispatch({
+    type: types.message.FETCH_REQUEST,
+  });
+
+  return api.fetchMessage()
+    .then((response) => dispatch({
+      type: types.message.FETCH_SUCCESS,
+      payload: {
+        message: response.message,
+      },
+    }),
+    (err) => dispatch({
+      type: types.message.FETCH_FAILURE,
+      payload: err,
+    }));
+};
